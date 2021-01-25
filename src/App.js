479f398647611eaa86311ce17f4e46e002008ff5
kento.uoi <fishperson19@gmail.com>
2021-01-25 23:01:22 +0900

    2-dPostFunction

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+// dbからデータを取得する
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
 
+  // console.log('render', notes.length, 'notes')
+
+// ノートを追加して、dbにpostする
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      console.log(response)
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
+// form中身の変更内容を取得
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+    console.log(id + 'の重要度を変えるで〜')
   }
 
   const notesToShow = showAll
@@ -33,14 +63,20 @@ const App = (props) => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? '重要なん' : '全部' }
         </button>
-      </div>      
+      </div>
       <ul>
+        {/* notesToShowを実行 */}
         {notesToShow.map((note, i) => 
-          <Note key={i} note={note} />
+          <Note
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
+      {/* submitをクリックするとaddNoteを実行する */}
       <form onSubmit={addNote}>
         <input
           value={newNote}
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? '価値をなくすで〜' : '重要にするで〜'
+  
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
   )
 }
 
